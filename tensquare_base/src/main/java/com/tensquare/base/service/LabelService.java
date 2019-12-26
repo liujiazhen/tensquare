3829888d88acf5f9bd420d6eebaf8508f4969860
liujiazhen <liujiazhen@live.cn>
2019-12-26 09:30:59 +0800

    spring data jpa

--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -11,10 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import util.IdWorker;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,56 +45,53 @@ public class LabelService {
     }
 
     public List<Label> findSearch(Label label) {
-        return labelDao.findAll(new Specification<Label>() {
-            /**
-             * @param root 根对象，也就是要把条件封装到哪个对象中。where 类名=label.getid
-             * @param query 封装的都是查询关键字，比如group by order by 等
-             * @param cb 用来封装条件对象的,如果直接返回null，表示不需要任何条件
-             * @return Predicate
-             */
-            @Override
-            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-                //new一个list集合。来存放所有的条件
-                List<Predicate> list = new ArrayList<>();
-                if (label.getLabelname() != null && !"".equals(label.getLabelname())) {
-                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");// labelname like "%小明%"
-                    list.add(predicate);
-                }
-                if (label.getState() != null && !"".equals(label.getState())) {
-                    Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());// state = "1"
-                    list.add(predicate);
-                }
-                //new一个数组作为最终返回值的条件
-                Predicate[] parr = new Predicate[list.size()];
-                //把list直接转成数组
-                parr = list.toArray(parr);
-                return cb.and(parr);// where labelname like "%小明%" and state = "1"
+        return labelDao.findAll((Specification<Label>) (root, query, cb) -> {
+            // new一个list集合。来存放所有的条件
+            List<Predicate> list = new ArrayList<>();
+            if (label.getLabelname() != null && !"".equals(label.getLabelname())) {
+                Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");// labelname like "%小明%"
+                list.add(predicate);
             }
+            if (label.getState() != null && !"".equals(label.getState())) {
+                Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());// state = "1"
+                list.add(predicate);
+            }
+            // new一个数组作为最终返回值的条件
+            Predicate[] parr = new Predicate[list.size()];
+            //把list直接转成数组
+            parr = list.toArray(parr);
+            return cb.and(parr);// where labelname like "%小明%" and state = "1"
+
         });
     }
 
     public Page<Label> pageQuery(Label label, int page, int size) {
-        //封装分页对象
+        // 封装分页对象
         Pageable pageable = PageRequest.of(page - 1, size);
-        return labelDao.findAll(new Specification<Label>() {
-            @Override
-            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-                //new一个list集合。来存放所有的条件
-                List<Predicate> list = new ArrayList<>();
-                if (label.getLabelname() != null && !"".equals(label.getLabelname())) {
-                    Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");// labelname like "%小明%"
-                    list.add(predicate);
-                }
-                if (label.getState() != null && !"".equals(label.getState())) {
-                    Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());// state = "1"
-                    list.add(predicate);
-                }
-                //new一个数组作为最终返回值的条件
-                Predicate[] parr = new Predicate[list.size()];
-                //把list直接转成数组
-                parr = list.toArray(parr);
-                return cb.and(parr);// where labelname like "%小明%" and state = "1"
+        return labelDao.findAll((Specification<Label>) (root, query, cb) -> {
+            List<Predicate> list = new ArrayList<>();
+            if (label.getLabelname() != null && !"".equals(label.getLabelname())) {
+                Predicate predicate = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");
+                list.add(predicate);
             }
+            if (label.getState() != null && !"".equals(label.getState())) {
+                Predicate predicate = cb.equal(root.get("state").as(String.class), label.getState());
+                list.add(predicate);
+            }
+            Predicate[] parr = new Predicate[list.size()];
+            parr = list.toArray(parr);
+            return cb.and(parr);
         }, pageable);
+    }
+
+    public Page<Label> find(Label label, int page, int size) {
+        PageRequest pageable = PageRequest.of(page - 1, size);
+        labelDao.findAll((Specification<Label>) (root, query, cb) -> {
+
+            Predicate predicate1 = cb.like(root.get("labelname").as(String.class), label.getLabelname() + "%");
+
+            return predicate1;
+        }, pageable);
+        return null;
     }
 }
