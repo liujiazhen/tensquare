716f7d32732b27982f28575fa3e8bcecbdc75eec
liujiazhen <liujiazhen@live.cn>
2020-01-19 16:19:46 +0800

    增加注释

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * 标签Controller
+ * @author LiuJiazhen
+ */
+
 @RestController
 @CrossOrigin
 @RequestMapping("/label")
@@ -18,22 +23,42 @@ public class LabelController {
     @Autowired
     private LabelService labelService;
 
+    /**
+     * 获取所有标签 get方法
+     * @return result
+     */
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll() {
         return new Result(true, StatusCode.OK, "查询成功", labelService.findAll());
     }
 
+    /**
+     * 根据ID获取标签 get方法
+     * @param labelId 标签id
+     * @return result
+     */
     @RequestMapping(value = "/{labelId}", method = RequestMethod.GET)
     public Result findById(@PathVariable("labelId") String labelId) {
         return new Result(true, StatusCode.OK, "查询成功", labelService.findById(labelId));
     }
 
+    /**
+     * 新增标签
+     * @param label 标签信息
+     * @return result
+     */
     @RequestMapping(method = RequestMethod.POST)
     public Result save(@RequestBody Label label) {
         labelService.save(label);
         return new Result(true, StatusCode.OK, "添加成功");
     }
 
+    /**
+     * 更新标签
+     * @param labelId 标签id
+     * @param label 标签内容
+     * @return result
+     */
     @RequestMapping(value = "/{labelId}", method = RequestMethod.PUT)
     public Result update(@PathVariable String labelId, @RequestBody Label label) {
         label.setId(labelId);
@@ -41,18 +66,35 @@ public class LabelController {
         return new Result(true, StatusCode.OK, "更新成功");
     }
 
+    /**
+     * 根据ID删除标签
+     * @param labelId 标签ID
+     * @return result
+     */
     @RequestMapping(value = "/{labelId}", method = RequestMethod.DELETE)
     public Result deleteById(@PathVariable String labelId) {
         labelService.deleteById(labelId);
         return new Result(true, StatusCode.OK, "删除成功");
     }
 
+    /**
+     * 条件查询标签
+     * @param label 标签信息
+     * @return result
+     */
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     public Result findSearch(@RequestBody Label label) {
         List<Label> list = labelService.findSearch(label);
         return new Result(true, StatusCode.OK, "查询成功", list);
     }
 
+    /**
+     * 条件分页查询标签
+     * @param label 标签信息
+     * @param page 页码
+     * @param size 页大小
+     * @return result
+     */
     @RequestMapping(value = "/search/{page}/{size}", method = RequestMethod.POST)
     public Result pageQuery(@RequestBody Label label, @PathVariable int page, @PathVariable int size) {
         Page<Label> pageData = labelService.pageQuery(label, page, size);
